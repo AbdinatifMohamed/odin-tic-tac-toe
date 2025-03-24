35af4b1c9db5ba9f7e4255c03e17c31ddbb40731
Abdinatif Mohamed <197330086+AbdinatifMohamed@users.noreply.github.com>
2025-03-24 18:23:44 -0500

    finished up the tic tac toe screencontroller

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const domBoard = document.querySelector(".spaces");
+const status = document.querySelector(".status");
+const cols = document.querySelectorAll(".col");
+const playeroneinput = document.querySelector("#playeronename");
+const playertwoinput = document.querySelector("#playertwoname");
+
+
 function Gameboard() {
     const rows = 3;
     const columns = 3;
@@ -50,10 +57,10 @@ function Gameboard() {
 
         //  (0, 1)  (1, 1)  (2, 2)
         //  (0, 2) ( 1, 1) (2, 0) 
-        if (board[0][1].getValue() === board[1][1].getValue() &&  board[1][1].getValue() === board[2][2].getValue() && board[0][1].getValue() === player) {
-            return true;
-        }
-        if (board[0][2].getValue() === board[1][1].getValue() &&  board[1][1].getValue() === board[2][0].getValue() && board[0][2].getValue() === player) {
+        if (
+            (board[0][0].getValue() === player && board[1][1].getValue() === player && board[2][2].getValue() === player) ||
+            (board[0][2].getValue() === player && board[1][1].getValue() === player && board[2][0].getValue() === player)
+        ) {
             return true;
         }
 
@@ -91,7 +98,8 @@ function Space() {
 
 function GameController(playerOneName = "Player One", playerTwoName = "Player Two"){
     const board = Gameboard();
-
+    let hasWinner = false;
+    let winner = null;
     const players = [
         {
           name: playerOneName,
@@ -110,13 +118,17 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
         activePlayer = activePlayer === players[0] ? players[1] : players[0]; 
     };
     const getActivePlayer = () => activePlayer;
+    const getHasWinner = () => hasWinner;
+    const getWinner = () => winner;
 
     const printNewRound = () => {
         board.printBoard();
         console.log(`${activePlayer.name}'s turn.`);
     }
 
+
     const playRound = (row, column) => {
+        if (hasWinner) return;
         console.log(`${activePlayer.name} attempts to place their mark on Row:${row}/Column:${column}`);
         
         const placed = board.placeMark(row, column, activePlayer.mark);
@@ -124,13 +136,12 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
         if (placed) {
             console.log(`${activePlayer.name} successfully placed their mark on Row:${row}/Column:${column}`);
 
-            const Won = board.isWinner(activePlayer.mark);
-            if (Won){
+            if (board.isWinner(activePlayer.mark)) {
                 console.log(`${activePlayer.name} has WON the game.`);
+                hasWinner = true;
+                winner = activePlayer.name;
             } else {
-                 console.log(`${activePlayer.name} WON: ${Won}`);
                 switchActivePlayer();
-                printNewRound();
             }
            
         } else {
@@ -143,9 +154,109 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
 
     return {
         playRound,
-        getActivePlayer
+        getActivePlayer,
+        getBoard: board.getBoard,
+        getHasWinner,
+        getWinner
     }
 }
 
 
-const game = GameController();
+function ScreenController(game){
+    const clearDomBoard = () => {
+        for (const row of domBoard.children) {
+            for (const col of row.children){
+                col.textContent = '';
+            }
+          }
+    }
+
+    const updateDisplay = (text) => {
+        status.textContent = text;
+    }
+
+    const renderBoard = () => {
+        const currentBoard = game.getBoard();
+        for (let row = 0; row < currentBoard.length; row++){
+            for (let col = 0; col < currentBoard[row].length; col++){
+               const rowcol = domBoard.querySelector(`#row-${row}`).querySelector(`#col-${col}`);
+               rowcol.textContent = currentBoard[row][col].getValue() === 0  ? '' : currentBoard[row][col].getValue() === 1  ? 'X' : 'O';
+            }
+        }
+    }
+    const updateScreen = () => {
+        const activePlayer = game.getActivePlayer();
+        clearDomBoard();
+        renderBoard();   
+        if (game.getHasWinner()) {
+            updateDisplay(`${game.getWinner()} has WON the game! 🎉`);
+        } else {
+            updateDisplay(`${activePlayer.name}'s turn`);
+        }
+    }
+
+    const clickHandlerBoard = (button) => {
+        console.log(game.getHasWinner()) 
+        const row = button.parentElement.dataset.row;
+        const col = button.dataset.col;
+        game.playRound(row, col);
+        updateScreen();
+    }
+    return {
+        clickHandlerBoard,
+        clearDomBoard
+    }
+}
+
+
+function newGame(){
+    const input1 = playeroneinput.value;
+    const input2 = playertwoinput.value;
+    
+    if (input1 === '' || input2 === '') {
+        // The input is empty
+        status.textContent = 'Empty names';
+        return null;
+      } else {
+        const game = GameController(input1, input2);
+        return game;
+      }
+
+}
+
+let game = null;
+let uiHandler = ScreenController(game);
+
+
+cols.forEach((col) => {
+    col.addEventListener('click', () => {
+        if (game === null) {
+            game = newGame();
+            uiHandler = ScreenController(game); 
+            uiHandler.clickHandlerBoard(col);
+        } else if(game.getHasWinner()) {
+            game = newGame();
+            uiHandler = ScreenController(game); 
+            uiHandler.clearDomBoard();
+            uiHandler.clickHandlerBoard(col);
+        } else {
+            uiHandler.clickHandlerBoard(col);
+        }
+    })
+})
+
+
+
+/*
+
+This here is the meat and potatoes of our screen controller. Here is what this method does, in order:
+
+Clear the DOM of the current board display by simply setting the .board div's text content to an empty string.
+Get the most up-to-date board from the game controller.
+Get the most up-to-date active player from the game controller.
+Render the player's turn in the .turn div.
+Render each grid square on the DOM
+I make sure to give each cell a data-attribute of column and set that value to the index of the cell in its row, so that when we click them in the future, we already have access to what column that cell is in.
+The cells are buttons, not divs. Why? In most cases, anything clickable should be a button or link. This enables those with accessability issues to still be able to use our site easily be tabbing and selecting with the keyboard.
+The purpose of this method is to refresh our screen whenever a change happens in our game. It will be called whenever the user interacts with the game, like to play a round.
+*/
